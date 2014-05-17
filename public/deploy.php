bb955d1f98c78efede914e2d043e602c9f5f9f6d
hatch2 <hiroaki@hatch2.com>
2014-05-17 15:29:31 +0900

    deployこれで

--- public/deploy.php
+++ public/deploy.php
@@ -1,10 +1,4 @@
 <?php
 
-$json = json_decode($_POST['payload'], true);
-
-// deployを実行する
-if(!empty($json['commits'][0]['branch'])) {
-  if ($json['commits'][0]['branch'] == 'production') exec('git checkout production;git pull');
-}
-
-
+// ごめんこれでゆるして
+exec('git checkout production;git pull');
