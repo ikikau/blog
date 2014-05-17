80239d2b070b8b63561d9089ce20f5dc11886794
hatch2 <hiroaki@hatch2.com>
2014-05-17 13:30:14 +0900

    deployテスト

--- public/deploy.php
+++ public/deploy.php
@@ -1,6 +1,12 @@
 <?php
-// exec('git pull');
 
-// $POST['commits']
+$json = json_decode($_POST);
 
-file_put_contents('./bitbucket_put.txt', $_POST);
+if(!empty($json['commits'][0]['branch'])) {
+  file_put_contents('./bitbucket_put.txt', $json['commits'][0]['branch']);
+  exec('git pull');
+} else {
+  file_put_contents('./bitbucket_put.txt', $_POST);
+}
+
+
