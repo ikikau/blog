afa311de6c9d89ce34100f1aa8e58aea2994ef60
hatch2 <hiroaki@hatch2.com>
2014-05-22 22:57:28 +0900

    compass init

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/wp-content/themes/ikikau/common/css"
+sass_dir = "public/wp-content/themes/ikikau/common/scss"
+images_dir = "public/wp-content/themes/ikikau/common/images"
+javascripts_dir = "public/wp-content/themes/ikikau/common/js"
+fonts_dir = "public/wp-content/themes/ikikau/common/font"
+
+output_style = :nested
+environment = :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass public/wp-content/themes/ikikau/common/scss scss && rm -rf sass && mv scss sass
